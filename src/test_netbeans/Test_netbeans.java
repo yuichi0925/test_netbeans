d9294b4486935c8e9d7baf23e41ff3dc6519fab3
祐一サブ <yuichi0925@a>
2017-12-02 16:27:42 +0900

    「おはよう」追加

--- src/test_netbeans/Test_netbeans.java
+++ src/test_netbeans/Test_netbeans.java
@@ -17,6 +17,7 @@ public class Test_netbeans {
     public static void main(String[] args) {
         System.out.println("こんにちは");
         System.out.println("こんばんわ");
+        System.out.println("おはよう");
     }
     
 }
