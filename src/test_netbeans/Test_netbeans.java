236aa2af06f73fc57ce186423991c81ced24a87b
祐一 <@home-PC>
2017-12-02 15:49:16 +0900

    「おはよう」から「こんばんわ」に変更

--- src/test_netbeans/Test_netbeans.java
+++ src/test_netbeans/Test_netbeans.java
@@ -15,7 +15,7 @@ public class Test_netbeans {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("おはよう");
+        System.out.println("こんばんわ");
     }
     
 }
