4725f0fc188248ea707ba6e8ca553128053da908
祐一サブ <yuichi0925@a>
2017-12-02 15:38:29 +0900

    「こんにちは」の表示

--- src/test_netbeans/Test_netbeans.java
+++ src/test_netbeans/Test_netbeans.java
@@ -15,7 +15,7 @@ public class Test_netbeans {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("こんにちは");
     }
     
 }
