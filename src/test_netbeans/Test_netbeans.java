9885c96af3193ca1170515360db2c6decf69e7ef
祐一サブ <yuichi0925@a>
2017-12-04 13:52:18 +0900

    初期化

--- src/test_netbeans/Test_netbeans.java
+++ src/test_netbeans/Test_netbeans.java
@@ -16,20 +16,5 @@ public class Test_netbeans {
      */
     public static void main(String[] args) {
         System.out.println("こんにちは");
-        System.out.println(sum(10,5));
-        System.out.println(sum1(10,5,5));
-        System.out.println(sum2(1,3,5,7));
     }
-    
-    static int sum(int i,int j){
-        return i+j;
-    }
-    static int sum1(int i,int j,int k){
-        return i+j+k;
-    }
-    static int sum2(int i,int j,int k,int l){
-        return i+j+k+l;
-    }
-    
-    
 }
